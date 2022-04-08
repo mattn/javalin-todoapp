2586c820c029fc3f2c23a55cc6c81a579b44f077
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-04-08 13:39:15 +0900

    DDL

--- src/main/java/io/github/mattn/todo/Main.java
+++ src/main/java/io/github/mattn/todo/Main.java
@@ -17,51 +17,56 @@ import java.util.List;
  * Main.
  */
 public class Main {
-	/**
-	 * Main function.
-	 * @param args arguments for program.
-	 * @throws Exception throws when something wrong.
-	 */
-	public static void main(String[] args) throws Exception {
-		SqlSessionFactory factory;
-		try (InputStream in = Main.class.getResourceAsStream("/mybatis-config.xml")) {
-			factory = new SqlSessionFactoryBuilder().build(in);
-		}
+    /**
+     * Main function.
+     * 
+     * @param args arguments for program.
+     * @throws Exception throws when something wrong.
+     */
+    public static void main(String[] args) throws Exception {
+        SqlSessionFactory factory;
+        try (InputStream in = Main.class.getResourceAsStream("/mybatis-config.xml")) {
+            factory = new SqlSessionFactoryBuilder().build(in);
+        }
+        try (SqlSession session = factory.openSession()) {
+            session.update("io.github.mattn.todo.create");
+            session.commit();
+        }
 
-		Javalin app = Javalin.create(config -> {
+        Javalin app = Javalin.create(config -> {
             config.enableWebjars();
         }).start(7000);
 
-		app.get("/api/todo", ctx -> {
-			ctx.res.setContentType("text/json");
+        app.get("/api/todo", ctx -> {
+            ctx.res.setContentType("text/json");
 
-			try (SqlSession session = factory.openSession()) {
-				List<Item> result = session.selectList("io.github.mattn.todo.select");
-				ctx.json(result);
-			}
-		});
-		app.post("/api/todo", ctx -> {
-			Item item = ctx.bodyAsClass(Item.class);
-			try (SqlSession session = factory.openSession()) {
-				session.insert("io.github.mattn.todo.insert", item);
-				session.commit();
-				ctx.status(201);
-			}
-		});
-		app.post("/api/todo/{id}", ctx -> {
-			Item item = ctx.bodyAsClass(Item.class);
-			item.setUpdatedAt(new Date());
-			try (SqlSession session = factory.openSession()) {
-				session.insert("io.github.mattn.todo.update", item);
-				session.commit();
-			}
-		});
-		app.delete("/api/todo/{id}", ctx -> {
-			try (SqlSession session = factory.openSession()) {
-				session.delete("io.github.mattn.todo.delete", Integer.parseInt(ctx.pathParam("id")));
-				session.commit();
-			}
-		});
-		app.get("/", new VueComponent("<todo-overview></todo-overview>"));
-	}
+            try (SqlSession session = factory.openSession()) {
+                List<Item> result = session.selectList("io.github.mattn.todo.select");
+                ctx.json(result);
+            }
+        });
+        app.post("/api/todo", ctx -> {
+            Item item = ctx.bodyAsClass(Item.class);
+            try (SqlSession session = factory.openSession()) {
+                session.insert("io.github.mattn.todo.insert", item);
+                session.commit();
+                ctx.status(201);
+            }
+        });
+        app.post("/api/todo/{id}", ctx -> {
+            Item item = ctx.bodyAsClass(Item.class);
+            item.setUpdatedAt(new Date());
+            try (SqlSession session = factory.openSession()) {
+                session.insert("io.github.mattn.todo.update", item);
+                session.commit();
+            }
+        });
+        app.delete("/api/todo/{id}", ctx -> {
+            try (SqlSession session = factory.openSession()) {
+                session.delete("io.github.mattn.todo.delete", Integer.parseInt(ctx.pathParam("id")));
+                session.commit();
+            }
+        });
+        app.get("/", new VueComponent("<todo-overview></todo-overview>"));
+    }
 }
